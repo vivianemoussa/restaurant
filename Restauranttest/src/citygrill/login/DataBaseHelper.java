03bd02f4ac634ae428f78afc7879a9265779d09f
ChVivo <ChVivo@ChVivo-PC>
2014-07-26 22:45:07 +0300

    add databasehelper

---
+++ Restauranttest/src/citygrill/login/DataBaseHelper.java
@@ -0,0 +1,18 @@
+package citygrill.login;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DataBaseHelper {
+
+	public DataBaseHelper(Context context, String databaseName, Object object,
+			int databaseVersion) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SQLiteDatabase getWritableDatabase() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
