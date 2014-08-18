e690725badd6e44acd644f6dde9eed3bb883acfd
ChVivo <ChVivo@ChVivo-PC>
2014-08-18 12:45:17 +0300

    open help page

--- Restauranttest/src/citygrill/login/help.java
+++ Restauranttest/src/citygrill/login/help.java
@@ -1,5 +1,18 @@
 package citygrill.login;
 
-public class help {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
 
+import com.order.R;
+
+public class help extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+       
+		super.onCreate(savedInstanceState);
+        setRequestedOrientation(0);
+     // initialise form widget
+        setContentView(R.layout.help);
+
+}
 }
