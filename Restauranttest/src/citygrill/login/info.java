e745c79ee48f92c843b690d31212ac61c4be8997
ChVivo <ChVivo@ChVivo-PC>
2014-08-18 12:44:30 +0300

    open info page

--- Restauranttest/src/citygrill/login/info.java
+++ Restauranttest/src/citygrill/login/info.java
@@ -1,5 +1,18 @@
 package citygrill.login;
 
-public class info {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
 
+import com.order.R;
+
+public class info extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+        Button help;
+		super.onCreate(savedInstanceState);
+        setRequestedOrientation(0);
+     // initialise form widget
+        setContentView(R.layout.info);
+
+}
 }
