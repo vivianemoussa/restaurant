d82ba923a2f2d55a4e8eb9bec2783d5ca77c042a
ChVivo <ChVivo@ChVivo-PC>
2014-08-27 13:15:33 +0300

    addition video to application

--- Restauranttest/src/citygrill/login/viewvideo.java
+++ Restauranttest/src/citygrill/login/viewvideo.java
@@ -7,9 +7,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.widget.VideoView;
 //import com.order.R;
+import com.example.testtab.R;
 
 public class viewvideo extends Activity  {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.videopage);
+		VideoView video = (VideoView) findViewById(R.id.video);
+		// Load and start the movie
+		Uri video1 = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.restaurantvideo);
+		video.setVideoURI(video1);
+		video.start();
+	}
 
+/*	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}*/
 
 
 }
