e1b97697e95311a4a7af4be233d9689f2c7d6267
ChVivo <ChVivo@ChVivo-PC>
2014-08-27 13:12:26 +0300

    create window to view video

---
+++ Restauranttest/src/citygrill/login/viewvideo.java
@@ -0,0 +1,15 @@
+package citygrill.login;
+
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.VideoView;
+//import com.order.R;
+
+public class viewvideo extends Activity  {
+
+
+
+}
