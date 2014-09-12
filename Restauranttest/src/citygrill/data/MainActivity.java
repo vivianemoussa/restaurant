ca504fe0a1a5e28c46b77ff799df1b092f32c4f4
ChVivo <ChVivo@ChVivo-PC>
2014-09-12 21:53:02 +0300

    data mainactivity

--- Restauranttest/src/citygrill/data/MainActivity.java
+++ Restauranttest/src/citygrill/data/MainActivity.java
@@ -1,7 +1,7 @@
 package citygrill.data;
-//import com.order.R;
-import com.example.testtab.R;
-
+import com.order.R;
+import android.view.View;
+import android.view.Window;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -11,14 +11,17 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	   // requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(0);
 		setContentView(R.layout.main);
+		
 	}
 
-/*	@Override
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}*/
+	}
 
 }
