b2fe8e11b0348fe2052ee6cb992afd3fa265e3de
ChVivo <ChVivo@ChVivo-PC>
2014-09-15 21:49:30 +0300

    default information

--- Restauranttest/src/citygrill/login/info.java
+++ Restauranttest/src/citygrill/login/info.java
@@ -1,19 +1,33 @@
 package citygrill.login;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-
-//import com.order.R;
-import com.example.testtab.R;
+import android.view.View.OnClickListener;
+import com.order.R;
 
 public class info extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
-        Button help;
-		super.onCreate(savedInstanceState);
-        setRequestedOrientation(0);
-     // initialise form widget
-        setContentView(R.layout.info);
 
-}
+		super.onCreate(savedInstanceState);
+		setRequestedOrientation(0);
+		// initialise form widget
+		setContentView(R.layout.info);
+		Button videov = (Button) findViewById(R.id.buttonvideo);
+
+		videov.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// starting new activity on button click
+				Intent i1 = new Intent(info.this, viewvideo.class);
+				startActivity(i1);
+
+			}
+		});
+
+	}
 }
